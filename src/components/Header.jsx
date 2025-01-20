c4897cec410a35cf77b4401fb089ec7beee0d273
EliaCasa <eperyaq@g.educaand.es>
2025-01-20 14:00:17 +0100

    Creacion repo

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import logo from "../assets/images/otros/logo_Diva.webp";
+import sol from "../assets/images/iconos/sol.webp";
+import luna from "../assets/images/iconos/luna.webp";
+import esp from "../assets/images/iconos/banderaEsp.webp";
+import eng from "../assets/images/iconos/reino-unido.webp";
+import de from "../assets/images/iconos/aleman.webp";
+import "../styles/Header.css";
+
+function Header() {
+    const [logged, setLogged] = useState(false); // Solo como ejemplo
+
+    return (
+        <header>
+            <div className="logo-container">
+                <img src={logo} alt="Logo de la diva" className="logo" />
+            </div>
+            <nav>
+                <ul>
+                    <li className='navbar'><img src={luna} alt="modo oscuro" /></li>
+                    <li className='navbar'>Inicio</li>
+                    <li className='navbar'>Sobre Nosotros</li>
+                    <li className='navbar'>Pedir cita piercing</li>
+                    <li className='navbar'>Pedir cita Tatoo</li>
+                    <li className='navbar'>{logged ? "Perfil" : "Iniciar sesión"}</li>
+                    <li className='navbar languages'>
+                        <img src={esp} alt="Spain flag" />
+                        <ul className="flags-dropdown">
+                            <li><img src={eng} alt="England flag" /></li>
+                            <li><img src={de} alt="German flag" /></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
