f9a2ce99427780f15cc9a6c6d7f901d760c04001
EliaCasa <eperyaq@g.educaand.es>
2025-02-10 13:00:44 +0100

    Modo oscuro hecho en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,18 +9,18 @@ function Header({darkmode, toggleTheme}) {
     return (
         <header className={darkmode ? "dark" : ""}>
             <article className="logo-container">
-                <img src={darkmode ? iconos.logo : iconos.logo_oscuro} alt="Logo de la diva" className="logo" />
+                <img src={darkmode ? iconos.logo : iconos.logo_oscuro} alt="Logo de la diva" className={`logo ${darkmode ? "dark" : ""}`} />
             </article>
             <nav>
                 <ul>
-                    <li className={`navbar ${darkmode ? "dark" : ""}`}><img src={darkmode ? iconos.sun : iconos.moon } alt={ darkmode ? "Modo claro" : "Modo oscuro"} onClick={toggleTheme} /></li>
+                    <li className={`navbar icono ${darkmode ? "dark" : ""}`}><img src={darkmode ? iconos.sun : iconos.moon } alt={ darkmode ? "Modo claro" : "Modo oscuro"} onClick={toggleTheme} /></li>
                     <li className={`navbar ${darkmode ? "dark" : ""}`}>Sobre Nosotros</li>
                     <li className={`navbar ${darkmode ? "dark" : ""}`}>Pedir cita piercing</li>
                     <li className={`navbar ${darkmode ? "dark" : ""}`}>Pedir cita Tatoo</li>
                     <li className={`navbar ${darkmode ? "dark" : ""}`}>{logged ? "Perfil" : "Iniciar sesión"}</li>
                     <li className={`navbar languages ${darkmode ? "dark" : ""}`}>
                         <img src={iconos.esp} alt="Spain flag" />
-                        <ul className="flags-dropdown">
+                        <ul className={`flags-dropdown ${darkmode ? "dark" : ""}`}>
                             <li><img src={iconos.eng} alt="England flag" /></li>
                             <li><img src={iconos.de} alt="German flag" /></li>
                         </ul>
