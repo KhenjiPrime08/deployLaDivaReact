c896d151ca4200617326a72f1f136eebdd52d21e
EliaCasa <eperyaq@g.educaand.es>
2025-05-27 22:01:10 +0200

    L

--- Front/src/config/config.js
+++ Front/src/config/config.js
@@ -1,2 +1,2 @@
-const API_URL = "back-production-a055.up.railway.app/api"; // Cambiar esto cuando suba a producción
+const API_URL = "back-production-15f1.up.railway.app/api"; // Cambiar esto cuando suba a producción
 export default API_URL;
